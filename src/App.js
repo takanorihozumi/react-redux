5632e456db347b3d8d65b299643a88b2041ee2e0
takanorihozumi <t.h032766@gmail.com>
2018-07-28 16:59:48 +0900

    use component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class App extends Component {
-  render() {
-    const greeting ="Hi";
-    //jsx ではclassではなくclassNameとかく（jsの予約語とかぶるから）
-    // const dom = <h1 className="">{greeting}</h1>;
-    return (
-      //reactは一つのタグで囲う必要がある（余計なdivを表示したくない場合にReact.Fragmentde
-      ）
-      <React.Fragment>
-        <label htmlFor="bar">bar</label>
-        <input type="text" onChange={()=> {console.log("ho")}} />
-      </React.Fragment>
+// class App extends Component {
+//   render() {
+//     const greeting ="Hi";
+//     //jsx ではclassではなくclassNameとかく（jsの予約語とかぶるから）
+//     // const dom = <h1 className="">{greeting}</h1>;
+//     return (
+//       //reactは一つのタグで囲う必要がある（余計なdivを表示したくない場合にReact.Fragmentde
+//       //）
+//       <React.Fragment>
+//         <label htmlFor="bar">bar</label>
+//         <input type="text" onChange={()=> {console.log("ho")}} />
+//       </React.Fragment>
+//
+//     );
+//   }
+// }
 
-    );
-  }
+const App = ()=> {
+  return (
+    <div>
+    <Cat/>
+    <Cat/>
+    <Cat/>
+    </div>
+  )
 }
-
+const Cat = ()=> {
+  return <div>Hi!</div>
+}
 export default App;
