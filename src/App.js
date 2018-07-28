1f9f4b9714a8490521e9026fcf87188d95703d35
takanorihozumi <t.h032766@gmail.com>
2018-07-28 16:13:48 +0900

    syntax

--- src/App.js
+++ src/App.js
@@ -2,7 +2,18 @@ import React, { Component } from 'react';
 
 class App extends Component {
   render() {
-    return <div><h1>Hello World</h1></div>;
+    const greeting ="Hi";
+    //jsx ではclassではなくclassNameとかく（jsの予約語とかぶるから）
+    // const dom = <h1 className="">{greeting}</h1>;
+    return (
+      //reactは一つのタグで囲う必要がある（余計なdivを表示したくない場合にReact.Fragmentde
+      ）
+      <React.Fragment>
+        <label htmlFor="bar">bar</label>
+        <input type="text" onChange={()=> {console.log("ho")}} />
+      </React.Fragment>
+
+    );
   }
 }
 
