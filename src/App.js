0d636bb9198c15cc5fc3b4fb8d098dcf93a34a09
takanorihozumi <t.h032766@gmail.com>
2018-08-05 22:05:13 +0900

    use state and setState

--- src/App.js
+++ src/App.js
@@ -1,32 +1,37 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, {Component} from 'react';
 
-const App = ()=> {
-  const profiles =[
-    {name: "Taro", age:10},
-    {name: "Hanako", age:5},
-    {name: "hoz"}
-  ]
-  return (
-    <div>
-    {
-      profiles.map((profile, index) =>{
-        console.log('hoz'+index);
-        return <User name={profile.name} age={profile.age} hoz={"hoz"} key={index}/>
-      })
+const App = ()=> (<Counter></Counter>)
 
-    }
-    </div>
-  )
-}
-const User = (props)=> {
-  return <div>Hi!I am {props.name}, and {props.age}才！, and {props.hoz}</div>
-}
+class Counter extends Component {
+  //初期化処理（instance作成時に呼ばれる）
+  constructor(props){
+    super(props)
+    console.log(this.state)
+    this.state = {count:0,hoz:"hoz"}
+  }
+  handlePlusButton = () =>{
+    //state設定用のでファクタスタンダード(stateを直接いじってはだめ)
+    //状態を変えて、DOMも再度renderしたいため。setStateではrenderも実行されてる
+    this.setState({count: this.state.count + 1})
+  }
+  handleMinusButton =()=>{
+    this.setState({count:this.state.count -1})
+  }
+  render(){
+    console.log("render実行")
 
-//PropTypesで型を定義する
-User.propTypes = {
-  name: PropTypes.string,
-  age: PropTypes.number.isRequired
+    return (
+      <React.Fragment>
+        <div id="test">count: { this.state.count},{this.state.hoz }</div>
+        <button onClick={this.handlePlusButton}>+1</button>
+        <button onClick={this.handleMinusButton}>-1</button>
+      </React.Fragment>
+    )
+  }
 }
+//stateはclassコンポーネントの内部で利用するもの
+//propsはimmutableな値
+//stateはmutableで親から子に値を渡す
+
 
 export default App;
