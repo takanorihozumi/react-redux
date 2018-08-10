c8c7df0bb8be202c1fe2e84aee79d2542792b698
takanorihozumi <t.h032766@gmail.com>
2018-08-10 17:40:58 +0900

    fix bug

--- src/reducers/count.js
+++ src/reducers/count.js
@@ -3,13 +3,13 @@ import { INCREMENT, DECREMENT } from '../actions'
 //stateの初期値をオブジェクトとして定義
 const initialState = { value: 0 }
 
-export default = (state = initialState, action) => {
+export default (state = initialState, action) => {
   switch (action.type) {
     case INCREMENT:
-      return { state.value +1 }
+      return { value:state.value +1 }
     case INCREMENT:
-      return { state.value +1 }
+      return { value:state.value +1 }
     default:
-      return { state.value }
+      return { value:state.value }
   }
 }
